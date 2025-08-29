a9e67a0cd4e63031fc617d8594722dafb0c41271
MdMahfujHossen.Pr <mdmahfujhossen.pr@gmail.com>
2025-08-29 16:18:45 +0600

    TeamSection , TestimonialSection and ContactSection Compelete Now

--- app/components/ContactSection.jsx
+++ app/components/ContactSection.jsx
@@ -4,8 +4,11 @@ import contactImg from "@/public/images/contact.png";
 
 export default function ContactSection() {
   return (
-    <section className="py-16 px-6 max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center">
-      <ContactForm />
+    <section className="py-16 px-6 max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center bg-[#F3F3F3]">
+      <div className="px-24">
+        <ContactForm />
+      </div>
+      
       <div className="flex justify-center md:justify-end">
         <Image src={contactImg} alt="Contact Image" width={450} height={400} className="rounded-lg" />
       </div>
